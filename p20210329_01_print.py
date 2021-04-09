87c07ee15506f3de77a7c9ec9585c56678f60e7b
yujaegeun <82205340+yujaegeun@users.noreply.github.com>
2021-04-09 17:13:34 +0900

    Add files via upload

---
+++ p20210329_01_print.py
@@ -0,0 +1,23 @@
+#주석 : 프로그램에 대한 설명을 적는다.
+#print함수 '''
+'''
+print(10+30)
+print(20-15)
+print(30*3)
+print(40/20)
+print(1+1)
+'''
+
+
+#매개변수,인자,인수
+print("헬로우" , end =",")
+print('헬로우')
+print('''헬로우''')
+print("my's")
+print('"seoul"')
+print('hello',2021,'python')
+print('python' + ' is easy')
+print('python' * 3 )
+#이스케이프 문자 : \n -> 다음줄로 가라는 명령어(개행)
+print('python\n' * 3 )
+
